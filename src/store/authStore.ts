1086437d79b58637dcc35111f68a9dd44912b27b
Enzo Luo <duenzo1010@gmail.com>
2022-09-05 23:14:59 +0800

    fix: filename bug

---
+++ src/store/authStore.ts
@@ -0,0 +1,32 @@
+import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+export type AuthState = {
+  nickname: string;
+  email: string;
+  token: string;
+};
+
+export interface IAuthStore {
+  authState: AuthState | null;
+  setAuthState: (authState: AuthState) => void;
+  clearAuthState: () => void;
+  isAuthorize: () => boolean;
+}
+
+export const useAuthStore = create<IAuthStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        authState: null,
+        setAuthState: (authState) => set({ authState }),
+        clearAuthState: () => set({ authState: null }),
+        isAuthorize: () => !!get().authState?.token,
+      }),
+      {
+        name: 'authStore',
+        getStorage: () => sessionStorage,
+      }
+    )
+  )
+);
